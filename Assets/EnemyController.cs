71b64d44316d6c0938312c8696f8e78cecd01669
NorthCopibara <ElshinA;elsandr@mail.ru>
2019-12-16 17:54:26 +0300

    Temp_2

--- Assets/EnemyController.cs
+++ Assets/EnemyController.cs
@@ -1,18 +1,27 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 
 public class EnemyController : MonoBehaviour
 {
-    [SerializeField] private float _forceUp;
-    [SerializeField] private float _forceMovement;
+    [SerializeField] private float _forceUp; //Enemy
+    [SerializeField] private float _forceMovement; //Enemy
+
+    private int _hialth = 100;
+
     Rigidbody _rbEnemy;
+    public Transform _targetMovement; //Заменить на конткретные координаты
 
-    public Transform _targetMovement;
+    private bool _upCondition; //Блок поворота
+    RaycastHit hit;
+    Ray ray;
 
-    private bool _upCondition;
-
+    private void OnMouseDown()
+    {
+        if (Physics.Raycast(ray, out hit))
+        {
+            Destroy(hit.transform.gameObject);
+        }
+    }
     private void Start()
     {
         _rbEnemy = GetComponent<Rigidbody>();
@@ -20,22 +29,20 @@ public class EnemyController : MonoBehaviour
 
     private void Update()
     {
-        //transform.position = Vector2.MoveTowards(transform.position, _targetMovement.position, _forceMovement * Time.deltaTime);
-
-        // Тот самый поворот
-        // вычисляем разницу между текущим положением и положением мыши
-        Vector3 difference = _targetMovement.position - transform.position;
-        difference.Normalize();
-        // вычисляемый необходимый угол поворота
-        float rotation_y = Mathf.Atan2(difference.x, difference.z) * Mathf.Rad2Deg;
-        // Применяем поворот вокруг оси Z
-        transform.rotation = Quaternion.Euler(0f, rotation_y, 0f);
-
+        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         if (!_upCondition)
         {
+            Vector3 difference = _targetMovement.position - transform.position;
+            difference.Normalize();
+            float rotation_y = Mathf.Atan2(difference.x, difference.z) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.Euler(0f, rotation_y, 0f);
+
+
             _rbEnemy.transform.Translate(Vector3.forward * _forceMovement * Time.deltaTime);
         }
     }
+
+   
 
     private void OnCollisionEnter(Collision collision)
     {
---
+++ Assets/Player.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    RaycastHit hit;
+    Ray ray;
+
+    private void Update()
+    {
+        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+    }
+
+    private void OnMouseDown()
+    {
+        if (Physics.Raycast(ray, out hit))
+        {
+            Destroy(hit.transform.gameObject);
+        }
+    }
+}
